387c6e08a256da372a78db6d979a82d0dd5c6e32
Kai Nguyen <nguyenduckhai8101@gmail.com>
2023-10-14 12:25:31 +0700

    add https api

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-const socket = io("http://api.airchecker.online");
+const socket = io("https://api.airchecker.online");
 
 const initChart = () => {
 	const myChartCanvas = document.getElementById("myChart");
