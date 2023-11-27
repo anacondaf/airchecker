ab6045a1ffc71da2cd9dbb1d61c0ac723ff9f9a3
Kai Nguyen <nguyenduckhai8101@gmail.com>
2023-11-27 21:08:52 +0700

    add more pollutants

--- backend/src/enums/pollutant_info.js
+++ backend/src/enums/pollutant_info.js
@@ -2,10 +2,16 @@ const tvoc = "tvoc";
 const o3 = "o3";
 const co = "co";
 const pm25 = "pm25";
+const co2 = "co2";
+const temp = "temperature";
+const humid = "humidity";
 
 module.exports = {
-	tvoc: tvoc,
-	o3: o3,
-	co: co,
-	pm25: pm25,
+	tvoc,
+	o3,
+	co,
+	pm25,
+	co2,
+	temp,
+	humid,
 };
