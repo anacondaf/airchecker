6b7e5c303048325e50b4b36b5b4a57b345f603e5
Khang Cao <nguyenduckhai8101@gmail.com>
2024-01-04 12:40:51 +0700

    add analytics page

---
+++ dashboard/src/pages/Analytics.jsx
@@ -0,0 +1,12 @@
+import "../styles/dashboard.style.css";
+import Chart from "../components/Dashboard/Chart";
+
+function Analytics() {
+	return (
+		<div className="dashboard">
+			<Chart />
+		</div>
+	);
+}
+
+export default Analytics;
